9593610f30d9f92a0c781aa01df622ee6bb30f3c
mydude9000 <jesus.ramirez5618@gmail.com>
2025-04-01 21:28:11 -0500

    Update Project2RoughDraft.cpp

--- Project2RoughDraft.cpp
+++ Project2RoughDraft.cpp
@@ -47,6 +47,43 @@ string getName() {
     return name;
 }
 
+//Function to generate a random index of 2D array
+int genRandomIndex() {
+    return rand () % SIZE; 
+}
+
+//Shuffle function that generates a random index number and swaps the values 
+void shuffle (int cards[][SIZE]) {
+    for(int i = 0; i < 15; ++i) {
+        int x1 = genRandomIndex();
+        int y1 = genRandomIndex();
+        int x2 = genRandomIndex();
+        int y2 = genRandomIndex();
+        swap(cards[x1][y1], cards[x2][y2]);
+    }
+}
+
+//Initialize function that holds cards arrangement and holds cards status (face up or face down)
+void initialize(int cardsArray[][SIZE], int cardsStatus[][SIZE]) {
+    int nums[16];
+    for (int i = 0; i < 8; ++i) {
+        nums[2*i] = i+1;
+        nums[2*i+1] = i+1;
+    }
+    for (int i = 0; i <15; ++i) {
+        int a = rand() % 16;
+        int b = rand() % 16;
+        swap(nums[a], nums[b]);
+    }
+    int index = 0;
+    for (int i = 0; i < SIZE;  ++i) {
+        for (int j = 0; j < SIZE; ++j){
+            cardsArray[i][j] = nums[index++];
+            cardsStatus[i][j] = 0;
+        }
+    }
+    shuffle(cardsArray);
+}
 
 
 
